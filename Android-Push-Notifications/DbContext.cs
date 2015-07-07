b64b928b4e43710b1976be00d26d86aeb35b7ab5
annmasalskaya <mas.anna25@gmail.com>
2015-07-07 15:36:02 +0300

    Added work with database. Added views.

---
+++ Android-Push-Notifications/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+public class DbContext : System.Data.Entity.DbContext
+{
+    
+    public System.Data.Entity.DbSet<Android_Push_Notifications.Models.Device> Devices { get; set; }
+
+    public System.Data.Entity.DbSet<Android_Push_Notifications.Models.User> Users { get; set; }
+
+}
--- Android-Push-Notifications/Models/Device.cs
+++ Android-Push-Notifications/Models/Device.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -13,7 +12,6 @@ namespace Android_Push_Notifications.Models
         public int Id { get; set; }
 
         [Required]
-        [ForeignKey("UserId")]
         [Display(Name = "UserId")]
         public int userID { get; set; } 
         
