e72f5da262f69831d32295a9f4070f2832845b45
annmasalskaya <mas.anna25@gmail.com>
2015-07-08 13:10:33 +0300

    Added initial db configuration

---
+++ Android-Push-Notifications/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+public class DbContext : System.Data.Entity.DbContext
+{
+   
+  
+
+    public System.Data.Entity.DbSet<Android_Push_Notifications.Models.User> Users { get; set; }
+
+    public System.Data.Entity.DbSet<Android_Push_Notifications.Models.Device> Devices { get; set; }
+
+}
