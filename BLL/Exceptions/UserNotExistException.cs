610242127bb6ccbdd281177d12a70b1eb71c4ff4
annmasalskaya <mas.anna25@gmail.com>
2015-07-10 15:05:14 +0300

    Added custom exception

---
+++ BLL/Exceptions/UserNotExistException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Exceptions
+{
+    class UserNotExistException : Exception
+    {
+        public UserNotExistException() :base() {}
+        public UserNotExistException(string message) : base(message) { }
+    }
+}
