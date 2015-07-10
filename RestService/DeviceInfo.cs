01d38c93f135a4589b83ce93be85695ed586d730
annmasalskaya <mas.anna25@gmail.com>
2015-07-10 13:15:34 +0300

    Renamed DS to RS

---
+++ RestService/DeviceInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestService
+{
+    [DataContract]
+    public class DeviceInfoContract
+    {
+        [DataMember]
+        public string UserLogin { get; set; }
+
+        [DataMember]
+        public string RegistrationId { get; set; }
+
+        [DataMember]
+        public string Imei { get; set; }
+    }
+}
---
+++ RestService/IRestService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestService
+{
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebInvoke(Method="POST",
+            ResponseFormat=WebMessageFormat.Json,
+            BodyStyle=WebMessageBodyStyle.Wrapped,
+            UriTemplate="RegisterDevice")]
+        string RegisterDevice(DeviceInfoContract deviceInfo);
+    }
+}
