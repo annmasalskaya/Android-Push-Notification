e0934830b32c1970eadced3f0c81ede992ae079d
annmasalskaya <mas.anna25@gmail.com>
2015-07-10 19:33:20 +0300

    Update MVC, DAL, DLL structure

---
+++ MVC/Models/DeviceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using BLL.Entites;
+
+namespace Android_Push_Notifications.Models
+{
+    public class DeviceModel: Device
+    {
+        [Required]
+        [Display(Name = "Registration Id")]
+        [DataType(DataType.Text)]
+        public string RegistrationId { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Imei { get; set; }
+    }
+}
---
+++ MVC/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using BLL.Entites;
+
+namespace Android_Push_Notifications.Models
+{
+    public class UserModel: User
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Length must be from 3 to 50 symbols")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
