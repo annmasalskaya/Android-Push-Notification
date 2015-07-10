597e919d6bf43337d6e9e21b20e4914ec4c9f840
astepanischev <artem.stepanischev@hiqo-solutions.com>
2015-07-10 17:07:24 +0600

    Implemented UserService

--- BLL/BLL/Services/UserService.cs
+++ BLL/BLL/Services/UserService.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using BLL.Interfaces.Services;
 using DAL.Entites;
+using DAL.Interfaces.Repositories;
+using DAL.Interfaces.UnitOfWork;
 
 namespace BLL.Services
 {
     public class UserService: IUserService
     {
+        IGenericRepository<User> _repository;
+        IUnitOfWork _unit;
+
+        public UserService(IGenericRepository<User> repository, IUnitOfWork unit)
+        {
+            _repository = repository;
+            _unit = unit;
+        }
+
         public User Create(User user)
         {
-            throw new NotImplementedException();
+            var createdUser = _repository.Create(user);
+            _unit.Commit();
+            return createdUser;
         }
     }
 }
