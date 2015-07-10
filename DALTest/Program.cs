e6852bc31acc09eff30a557affe20a09c8a945a9
astepanischev <artem.stepanischev@hiqo-solutions.com>
2015-07-10 15:18:11 +0600

    Updated Test code

--- DALTest/Program.cs
+++ DALTest/Program.cs
@@ -22,25 +22,44 @@ namespace DALTest
                 Console.WriteLine("Initializing Repository...");
                 var repository = new UserRepository(context);
 
+                Console.WriteLine("Enter user login to create a user:");
+                var login = Console.ReadLine();
                 Console.WriteLine("Creating User...");
-                var user = new User{
-                    Login = "FirstTestUser",
+                var user = new User
+                {
+                    Login = login,
                     Password = "12345678"
                 };
 
                 Console.WriteLine("User created: {0}", user);
 
                 Console.WriteLine("Saving user to repository...");
-                var responce = repository.Create(user);
-                Console.WriteLine("Repository responce: {0}", responce);
+                repository.Create(user);
                 
                 Console.WriteLine("Commiting changes...");
                 int unitResponce = unit.Commit();
                 Console.WriteLine("Number of saved rows: {0}", unitResponce);
+                Console.WriteLine("Done");
+
+                Console.WriteLine("All users: ");
+                var query = repository.GetAll();
+                foreach (var e in query)
+                {
+                    Console.WriteLine(e);
+                }
+
+                Console.WriteLine("Enter user login to find user:");
+                login = Console.ReadLine();
+
+                query = repository.GetBy(u => u.Login ==  login);
+                foreach (var e in query)
+                {
+                    Console.WriteLine("Repository responce: {0}", e);
+                }
+
+                
             }
-            Console.WriteLine("Done");
-            Console.WriteLine("Press Enter to exit");
-            Console.ReadLine();
+           
         }
     }
 }
