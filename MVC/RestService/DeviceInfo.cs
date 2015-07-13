b2f2bf3ad0bcf333b39d17fe3226a56b2d7583c9
annmasalskaya <mas.anna25@gmail.com>
2015-07-13 19:54:54 +0300

    Update hierarchy folder structure

---
+++ MVC/RestService/DeviceInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Android_Push_Notifications
+{
+    [DataContract]
+    public class DeviceInfoContract
+    {
+        [DataMember]
+        public string UserLogin { get; set; }
+
+        [DataMember]
+        public string RegistrationId { get; set; }
+
+        [DataMember]
+        public string Imei { get; set; }
+    }
+}
