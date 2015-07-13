08e8835401a7d90d5291bac436041bd126b35322
astepanischev <artem.stepanischev@hiqo-solutions.com>
2015-07-13 10:23:06 +0600

    Babaleila

---
+++ MVC/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Android_Push_Notifications.Models;
+using BLL.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL.Entites;
+
+namespace Android_Push_Notifications.Controllers
+{
+    public class UserController : Controller
+    {
+       // [Inject]
+        IDeviceService _deviceService;
+        //[Inject]
+        IUserService _userService;
+
+        public UserController(IDeviceService deviceService, IUserService userService)
+        {
+            _deviceService = deviceService;
+            _userService = userService;
+        }
+        
+        // GET: User
+        public ActionResult Index()
+        {
+            var users = _userService.GetAll();
+            var usersModel = AutoMapper.Mapper.Map<IEnumerable<User>, IEnumerable<UserModel>>(users);
+            return View(usersModel);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Login,Password")] UserModel userModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = AutoMapper.Mapper.Map<User>(userModel);
+                _userService.Create(user);
+                
+            }
+            return View("Create");
+        }
+
+        // GET 
+        public ActionResult Devices(UserModel userModel)
+        {
+            var user = AutoMapper.Mapper.Map<User>(userModel);
+            var devices = _deviceService.GetAllByUser(user);
+            var devicesModel = AutoMapper.Mapper.Map<IEnumerable<Device>, IEnumerable<DeviceModel>>(devices);
+            return View(devicesModel);
+        }      
+    }
+}
---
+++ MVC/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+using Ninject.Web.Common;
+using DependencyResolver;
+
+namespace Android_Push_Notifications.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Load(new DependencyResolverModule());
+        }
+    }
+}
